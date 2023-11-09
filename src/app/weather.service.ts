c9fce75df0abf414cd76717823078b7c9d9d9a26
zoomhawk <harshug921@gmail.com>
2023-11-09 16:24:44 +0530

    c4

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getweather(city: string, units: string, first_name:string) {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + first_name + '&APPID=794ee95e63c5a32aaf88cd813fa2e425&units=' + units);
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=794ee95e63c5a32aaf88cd813fa2e425&units=' + units);
   }
   getCountry(country: string) {
     return this.http.get('https://countriesnow.space/api/v0.1/countries');
